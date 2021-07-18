b2bbbe946fce9718e67c15dc32e980ac47117549
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-18 21:06:41 +0100

    Add icons to nav bar

--- App.tsx
+++ App.tsx
@@ -4,20 +4,42 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './pages/home/Home';
 import Search from './pages/search/Search';
 import Library from './pages/library/Library';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator tabBarOptions={{
+      <Tab.Navigator 
+      tabBarOptions={{
         tabStyle: {
-          justifyContent: 'center'
+          justifyContent: 'center', 
+          backgroundColor: '#2B2B2B'
+        },
+        labelStyle: {
+          fontSize: 14,
+          color: 'white',
+          fontWeight: 'bold'
         }
       }}>
-        <Tab.Screen name="Home" component={Home}/>
-        <Tab.Screen name="Search" component={Search} />
-        <Tab.Screen name="Your Library" component={Library} />
+        <Tab.Screen name="Home" component={Home} options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" size={size} color='white' />
+          )
+          }} />
+        <Tab.Screen name="Search" component={Search} options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" size={size} color='white' />
+          )
+          }} />
+        <Tab.Screen name="Your Library" component={Library} options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="book" size={size} color='white' />
+          )
+          }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
