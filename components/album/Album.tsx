c408915fa717b38ff8c9af926d2f5bd3d709955c
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-18 19:52:52 +0100

    Basic class setup

---
+++ components/album/Album.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+function Album(){
+    return(
+        <View>
+            <Text>Album</Text>
+        </View>
+    )
+}
+
+export default Album
---
+++ pages/library/Library.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+function Library(){
+    return(
+        <View>
+            <Text>Library</Text>
+        </View>
+    )
+}
+
+export default Library
