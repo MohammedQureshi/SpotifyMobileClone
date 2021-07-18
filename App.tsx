9f43e2aae93ffcdbdb2be62e9d90e67983c98a3c
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-18 20:18:45 +0100

    updated to justcontent center which fixed a bug when the screen width was lower than 520px.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './pages/home/Home';
@@ -11,8 +10,12 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+      <Tab.Navigator tabBarOptions={{
+        tabStyle: {
+          justifyContent: 'center'
+        }
+      }}>
+        <Tab.Screen name="Home" component={Home}/>
         <Tab.Screen name="Search" component={Search} />
         <Tab.Screen name="Your Library" component={Library} />
       </Tab.Navigator>
