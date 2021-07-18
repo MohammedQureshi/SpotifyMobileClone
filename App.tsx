75ce19d3c51149bfcee8389c9f0b64f2f4759284
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-18 21:19:16 +0100

    Use different icon pack

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './pages/home/Home';
 import Search from './pages/search/Search';
 import Library from './pages/library/Library';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -26,19 +26,19 @@ export default function App() {
         <Tab.Screen name="Home" component={Home} options={{
           tabBarLabel: 'Home',
           tabBarIcon: ({color, size}) => (
-            <Icon name="home" size={size} color={color} />
+            <Icon name="home-variant" size={size} color={color} />
           )
           }} />
         <Tab.Screen name="Search" component={Search} options={{
           tabBarLabel: 'Search',
           tabBarIcon: ({color, size}) => (
-            <Icon name="search" size={size} color={color} />
+            <Icon name="magnify" size={size} color={color} />
           )
           }} />
         <Tab.Screen name="Your Library" component={Library} options={{
           tabBarLabel: 'Your Library',
           tabBarIcon: ({color, size}) => (
-            <Icon name="book" size={size} color={color} />
+            <Icon name="bookshelf" size={size} color={color} />
           )
           }}/>
       </Tab.Navigator>
