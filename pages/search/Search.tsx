72b06914cb244f4f60a49b5b6e8af5397d3f87a4
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-18 22:05:07 +0100

    Setup search panel

--- pages/search/Search.tsx
+++ pages/search/Search.tsx
@@ -3,10 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 
 function Search(){
     return(
-        <View>
-            <Text>Search</Text>
+        <View style={{ backgroundColor: '#121212', flex:1}}>
+            <Text style={styles.header}>Search</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+        paddingTop: 20,
+        paddingLeft: 10,
+        color: 'white',
+        fontSize: 40,
+        fontWeight: 'bold'
+    }
+})
 
 export default Search
