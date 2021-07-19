798da7c74d1f7795422b7a47dcd78b912cd27dce
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-19 23:42:30 +0100

    a file where all the prefined colors used for the application are store that are shared between all styled components.

---
+++ lib/Colors.ts
@@ -0,0 +1,11 @@
+namespace Colors {
+    export const WHITE = '#fff';
+    export const GREY = '#535353';
+    export const LIGHT_GREY = '#b3b3b3';
+    export const BACKGROUND_COLOR = '#121212';
+    export class Card {
+        static readonly backgroundColor = '#181818';
+    }
+}
+
+export default Colors;
