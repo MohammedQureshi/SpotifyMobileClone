ee22ea9d04c2d8342f54cdc34c56bd8f3ff0a80e
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-20 00:13:21 +0100

    updated to use the new lib/Colors and centered icon and text.

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, Pressable } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Colors from '../../lib/Colors';
 
 function Button(){
     return(
             <Pressable style={styles.button}>
-                <Text style={styles.text}><Icon name="magnify" style={styles.icon} /> Artists, songs, or podcasts</Text>
+                <Icon name="magnify" style={styles.icon} /> 
+                <Text style={styles.text}>Artists, songs, or podcasts</Text>
             </Pressable>
     )
 }
@@ -17,19 +19,21 @@ const styles = StyleSheet.create({
         marginRight: 20,
         borderRadius: 4,
         elevation: 3,
-        backgroundColor: 'white',
+        backgroundColor: Colors.WHITE,
+        flexDirection: 'row',
     },
     text:{
         fontSize: 24,
         lineHeight: 40,
         fontWeight: 'bold',
         letterSpacing: 0.25,
-        color: '#535353',
+        color: Colors.GREY,
         paddingLeft: 15,
     },
     icon: {
+        marginLeft: 10,
         fontSize: 40,
-        color: '#535353',
+        color: Colors.GREY,
         flexDirection: 'column',
     }
 })
