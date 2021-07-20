fd9f959fb31c69f53daaef21952136965268a40b
Mohammed Qureshi <mohammedqureshi2998@gmail.com>
2021-07-20 11:10:59 +0100

    set up basic card

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,22 +1,42 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 interface params{
     passBackgroundColor: String;
+    title: String;
+    image: String;
 }
 
 function Card(params: any){
     return(
-        <View style={{backgroundColor: params.passBackgroundColor}}>
-            <Text>Hi</Text>
+        <View style={{backgroundColor: params.passBackgroundColor, width: '45%', padding: 10, margin: 10, borderRadius: 5, height: 100, overflow: "hidden"}}>
+            <Text style={styles.title}>{params.title}</Text>
+            <Image
+                style={styles.tinyLogo}
+                source={{
+                uri: params.image,
+                }}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    card: {
-        backgroundColor: 'red'
-    }
+    title: {
+        color:'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+        flexWrap: 'wrap',
+        width: 100
+    },
+    tinyLogo: {
+        width: 70,
+        height: 70,
+        position: 'absolute',
+        right: -20,
+        bottom: -5,
+        transform: [{rotate: '27deg'}],
+      },
 })
 
 export default Card
