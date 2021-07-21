93bfd57a0294b1213a2c8f54530b0082567ce56f
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-21 20:03:17 +0100

    updated to include slight style changes and wraps everything around a scroll view so the user can scrolldown the page as well as scroll sideways to view the artists and albums.

--- pages/home/Home.tsx
+++ pages/home/Home.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import CardPreview from '../../components/card/CardPreview';
+import SuggestionHeader from '../../components/suggestion/SuggestionHeader';
 import Colors from '../../lib/Colors';
+import { Buffer } from "buffer"
 
 function Home(){
     const styles = StyleSheet.create({
@@ -11,39 +13,79 @@ function Home(){
         },
         h1: {
             fontSize: 20,
-            marginBottom: 10
+            marginBottom: 20,
+            marginTop: 20,
+            fontWeight: 'bold'
         },
         background : {
             width: '100%',
             flex: 1,
             backgroundColor: Colors.BACKGROUND_COLOR,
             paddingTop: 20,
-            paddingBottom: 20
+            paddingBottom: 20,
+            overflow: 'scroll'
         },
         container: {
             width: '97%',
             marginLeft: 10,
-            alignSelf: 'flex-end',
+            alignSelf: 'flex-end'
         }
     });
     return(
         <View style={styles.background}>
+            <StatusBar hidden/> 
+            <ScrollView>
             <View style={styles.container}>
-                <StatusBar hidden/>
                 <View>
                     <Text style={[styles.text, styles.h1]}>Recently Played</Text>
                     <ScrollView horizontal={true}>
-                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" date={125125}/>
-                        <CardPreview.Element title="Hello World" type={CardPreview.Type.ARTIST} imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" date={125125}/>
-                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" date={125125}/>
-                        <CardPreview.Element title="Hello World" imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" date={125125}/>
-                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" date={125125}/>
-                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" date={125125}/>
-                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" date={125125}/>
-                        <CardPreview.Element title="Hello World" imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" date={125125}/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" type={CardPreview.Type.ARTIST} imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" />
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" type={CardPreview.Type.PODCAST} duration="11 min" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                    </ScrollView>
+                </View>
+
+                <View>
+                    <SuggestionHeader artist={{
+                        id: Buffer.from('AC DC', 'utf-8').toString('base64'),
+                        name: "AC DC",
+                        imageURL : "https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"
+                    }}/>
+                    <ScrollView horizontal={true}>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                    </ScrollView>
+                </View>
+
+
+                <View>
+                    <Text style={[styles.text, styles.h1]}>Workout</Text>
+                    <ScrollView horizontal={true}>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" type={CardPreview.Type.ARTIST} imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg" />
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" type={CardPreview.Type.PODCAST} duration="11 min" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius arcu eu nibh facilisis, non blandit augue volutpat. Morbi pharetra." imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
+                        <CardPreview.Element title="Hello World" imageURL="https://www.flat-e.com/flate5/wp-content/uploads/cover.jpg"/>
                     </ScrollView>
                 </View>
             </View>
+            </ScrollView>
         </View>
     )
 }
